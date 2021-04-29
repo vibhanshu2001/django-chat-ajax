ba9e36fc667d3329bc6c5ee10cd8d75d46813642
Vibhanshu Garg <v2001.garg@gmail.com>
2021-04-29 20:11:08 +0530

    Update chat

--- djangochat/urls.py
+++ djangochat/urls.py
@@ -21,6 +21,6 @@ from django.conf.urls import url
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('chat.urls')),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    # url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    # url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
